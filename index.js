6bd439bfb8107e2d6d47215eebf7bd33c1962100
Hamna-shah <hamnashah778@gmail.com>
2024-04-03 19:33:47 +0500

    added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import inquirer from "inquirer";
 const randonNumber = Math.floor(Math.random() * 6 + 1);
 const answers = await inquirer.prompt([{ name: "userGaussedNumber", type: "number", message: "please guess a number between 1-6" },]);
 if (answers.userGaussedNumber === randonNumber) {
-    console.log("congratulations you guessed right number");
+    console.log("congratulations you guessed correct number");
 }
 else {
     console.log("you guessed wrong number");
