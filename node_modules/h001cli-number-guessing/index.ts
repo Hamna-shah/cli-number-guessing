e545b1e292dd7985f10e3afe5fed8078b6dc215e
Hamna-shah <hamnashah778@gmail.com>
2024-04-03 19:19:49 +0500

    created number gaussing game

---
+++ node_modules/h001cli-number-guessing/index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randonNumber=Math.floor(Math.random() *6+1);
+const answers= await inquirer.prompt([{name:"userGaussedNumber",type:"number",message:"please guess a number between 1-6"},],)
+
+if(answers.userGaussedNumber===randonNumber){console.log("congratulations you guessed right number")}
+else{console.log("you guessed wrong number")}
+
+
+
+
+
+
+
+
+
